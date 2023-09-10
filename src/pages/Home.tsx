4a3424b82873fa96d7eea715b8874d8c34d3020d
Henrique <luiz@eway.dev>
2023-09-09 21:27:05 -0300

    💄 changle title name and spacing on home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,9 @@
 export function Home() {
   return (
-    <section className="h-screen bg-slate-50">
-      <h2>Home</h2>
+    <section className="h-full px-8 py-8 bg-slate-50">
+      <h2>
+        <strong>9 amigos</strong> encontrados
+      </h2>
     </section>
   )
 }
